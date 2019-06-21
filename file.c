78a7c59a18bcd61baac2bdb404881ceccdf23144
msdandrea <matheusdandrea@hotmail.com>
2019-06-20 22:42:28 -0300

    Oh god h€lp

---
+++ file.c
@@ -0,0 +1,264 @@
+////
+//// Created by matheus on 20/06/19.
+////
+//
+//
+//
+//
+//const int n_rec = 6; //recursos: 0 1 2 3 4 5
+//int *control_token = NULL;
+//int my_tokens[n_rec];
+//int needed_token[n_rec];
+//int **queue;
+//const int elected_node = 0;
+//enum messages {
+//    REQUEST_CONTROL_TOKEN = 0,
+//    CONTROL_TOKEN_MESSAGE = 1,
+//    INQ = 2,
+//    INQ_ACK1 = 3,
+//    INQ_ACK2 = 4,
+//    FINALIZE_MESSAGE = 9
+//};
+//
+//int self, parent, next, requesting, all_finalized, n_procs;
+//pthread_mutex_t access_mutex;
+//
+//void init() {
+//    printf("Iniciando o nó %d\n", self);
+//    all_finalized = 0;
+//    requesting = 0;
+//    pthread_mutex_init(&access_mutex, NULL);
+//    queue = malloc(sizeof(int *) * n_procs);
+//    for (int k = 0; k < n_rec; ++k) {
+//        my_tokens[k] = 0;
+//        needed_token[k] = 0;
+//    }
+//    for (int i = 0; i < n_procs; ++i) {
+//        queue[i] = malloc(sizeof(int) * n_rec);
+//        for (int j = 0; j < n_rec; ++j) {
+//            queue[i][j] = -1;
+//        }
+//    }
+//    if (self == elected_node) {
+//        control_token = malloc(sizeof(int) * n_rec);
+//        for (int i = 0; i < n_rec; ++i) {
+//            control_token[i] = -1;
+//        }
+//        parent = -1;
+//    } else {
+//        pthread_mutex_lock(&access_mutex);
+//        parent = elected_node;
+//    }
+//    next = -1;
+//}
+//
+//void release_cs();
+//
+//void send_token(int to);
+//
+//void send_request(int origin, int to);
+//
+//void send_finalize();
+//
+//void receive_request_cs(int sj);
+//
+//void receive_token();
+//
+//void request_cs();
+//
+//void *receive(void *nil);
+//
+//void request_cs() {
+//    requesting = 1;
+//    printf("Nó %d quer entrar na seção crítico.\n", self);
+//    if (parent != -1) {
+//        send_request(self, parent);
+//        parent = -1;
+//
+//        //espera token
+//        pthread_mutex_lock(&access_mutex);
+//
+//    }
+//    int random_sleep = (rand() % (3)) * 1000000;
+//    printf("Nó %d entrou na seção irá dormir por %d segundos\n", self, random_sleep / 1000000);
+//    usleep((__useconds_t) random_sleep);
+//    release_cs();
+//}
+//
+//
+//int has_all_res() {
+//    for (int i = 0; i < n_rec; ++i) {
+//        if (needed_token[i] && !my_tokens[i])
+//            return 0;
+//    }
+//    return 1;
+//}
+//
+//void request_resources(int *requested) {
+//    printf("Nó %d quer usar os recursos:", self);
+//    for (int i = 0; i < 3; ++i) {
+//        printf(" %d", requested[i]);
+//        needed_token[requested[i]] = 1;
+//    }
+//    printf("\n");
+//    if (!has_all_res(requested)) { //se ele não possui todos os recursos
+//        send_request(self, parent); //envia request
+//        pthread_mutex_lock(&access_mutex); //espera pra entrar
+//    }
+//    if(control_token && next!=-1){
+//        //envia control token
+//    }
+//    //seção crítica
+//
+//}
+//
+//void execute() {//while gera um numero entre 0 e total_procs se gerou "meu numero" faço o pedido, dorme por um segundo
+//    int count = 0;
+//    while (count != 3) {
+//        int random = (rand() % (n_procs));
+//        if (self == random) {
+//            int base_req = (rand() % (n_rec)); //primeiro recurso
+//            int top_req = (rand() % (n_rec)); //ultimo recurso
+//            top_req = top_req <= base_req ? base_req + 1 : top_req; // se top_req é menor que a base então ele é base +1
+//            int requested_res[n_rec]; // array de recursos que eu quero
+//            int j = 0;
+//            for (int i = base_req; i < top_req; ++i) {
+//                requested_res[j] = i; // quero o iésimo recurso a partir da base
+//                j++;
+//            }
+//            requested_res[j] = -1; //para de procurar recurso
+//            request_resources(requested_res);
+//            count++;
+//        }
+//        usleep(1 * 1000000);
+//    }
+//}
+//
+//int main(int argc, char **argv) {
+//    srand(rand());
+//    pthread_t rec_thread;
+//    MPI_Init(&argc, &argv);
+//
+//    //inicializa
+//    MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
+//    MPI_Comm_rank(MPI_COMM_WORLD, &self);
+//    init();
+//
+//    //levanta uma thread para ficar escutando requests
+//    pthread_create(&rec_thread, NULL, receive, NULL);
+//
+//    //executa
+//    execute();
+//
+//    //finaliza
+//    send_finalize();
+//    pthread_join(rec_thread, NULL);
+//    MPI_Finalize();
+//    return 0;
+//}
+//
+//
+//void send_inquire(int resources[3], int node) {
+//
+//}
+//
+//void receive_ack_1(int tokens[3]){
+//    for (int i = 0; i < 3; ++i) {
+//        if (tokens[i] != -1){
+//            my_tokens[tokens[i]] = 1;
+//        }
+//    }
+//}
+//
+//void receive_token(int *control_token) {
+//    for (int i = 0; i < n_rec; ++i) {
+//        if (needed_token[i] && control_token[i] == -1) {//pegue todos os tokens que precisa e não tem
+//            control_token[i] = self;
+//            my_tokens[i] = 1;
+//        } else if (control_token[i] == self && !needed_token[i]) {//devolva todos os que tem e não precisa
+//            my_tokens[i] = 0;
+//            control_token[i] = -1;
+//        }
+//    }
+//    if (!has_all_res()) {
+//        for (int i = 0; i < n_procs; ++i) {
+//            int resources[3] = {-1, -1, -1};
+//            int count = 0;
+//            for (int j = 0; j < n_rec; ++j) {
+//                if (control_token[j] == i && needed_token[j]) {
+//                    resources[count] = j;
+//                    count++;
+//                }
+//            }
+//            if (resources[0] != -1) {
+//                send_inquire(resources, i);
+//            }
+//        }
+//    } else {
+//        pthread_mutex_unlock(&access_mutex);
+//    }
+//}
+//
+//void receive_finalize() {
+//    all_finalized++;
+//}
+//
+//void receive_request_cs(int sj) {
+//    if (parent == -1) {
+//        if (requesting) {
+//            printf("%d é next de %d\n", sj, self);
+//            next = sj;
+//        } else {
+//            send_token(sj);
+//        }
+//    } else {
+//        send_request(sj, parent);
+//    }
+//    parent = sj;
+//    printf(" Owner de %d é %d\n", self, parent);
+//}
+//
+//void release_cs() {
+//    printf("Nó %d liberando seção crítica\n", self);
+//    pthread_mutex_unlock(&access_mutex);
+//    requesting = 0;
+//    if (next != -1) {
+//        send_token(next);
+//        pthread_mutex_lock(&access_mutex);
+//        next = -1;
+//    }
+//}
+//
+////função pra thread de receive
+//void *receive(void *nil) {
+//    while (all_finalized != n_procs) {
+//        MPI_Status st;
+//        int received;
+//        MPI_Recv(&received, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &st);
+//        printf("Look! Nó %d recebeu mensagem %d originária do nó %d através do nó %d\n", self, st.MPI_TAG,
+//               received, st.MPI_SOURCE);
+//        if (st.MPI_TAG == REQUEST_CONTROL_TOKEN) {
+//            receive_request_cs(received);
+//        } else if (st.MPI_TAG == CONTROL_TOKEN_MESSAGE) {
+////            receive_token();
+//        } else if (st.MPI_TAG == FINALIZE_MESSAGE) {
+//            receive_finalize();
+//        } else {
+//            printf("ERROR UNKNOWN MESSAGE RECEIVED: %c", st.MPI_TAG);
+//        }
+//    }
+//}
+//
+//void send_finalize() {
+//    for (int to = 0; to < n_procs; to++) {
+//        MPI_Send(&self, 1, MPI_INT, to, FINALIZE_MESSAGE, MPI_COMM_WORLD);
+//    }
+//}
+//
+//void send_token(int to) {
+//    MPI_Send(&self, 1, MPI_INT, to, CONTROL_TOKEN_MESSAGE, MPI_COMM_WORLD);
+//}
+//
+//void send_request(int origin, int to) {
+//    MPI_Send(&origin, 1, MPI_INT, to, REQUEST_CONTROL_TOKEN, MPI_COMM_WORLD);
+//}
--- main.c
+++ main.c
@@ -1,34 +1,60 @@
+
 #include <stdio.h>
 #include <mpi.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
 
-const int n_rec = 6; //recursos: 0 1 2 3 4 5
-int *control_token = NULL;
-int my_tokens[n_rec];
-int **queue;
-const int elected_node = 0;
 enum messages {
-    REQUEST_MESSAGE = 0,
+    REQUEST_CONTROL_TOKEN = 0,
     CONTROL_TOKEN_MESSAGE = 1,
     INQ = 2,
     INQ_ACK1 = 3,
     INQ_ACK2 = 4,
-    FINALIZE_MESSAGE = 9
+    FINALIZE_MESSAGE = 9,
+    ACK_FINALIZE = 10
 };
 
-int self, parent, next, requesting, all_finalized, n_procs;
+
+const int n_rec = 6; //recursos: 0,1,2,3,4,5
+int pv[7][3] = {
+        {0, 2, 4},
+        {1, 3, 5},
+        {6, 4, 5},
+        {2, 3, 5},
+        {0, 1, 3},
+        {0, 1, 2},
+        {0, 3, 5}
+};
+
+int *my_tokens;
+int *control_token = NULL;
+const int elected_node = 0;
+int *needed_tokens;
+int **queue;
+int n_proc;
+int self, parent, next, using_token, all_finalized, n_procs;
 pthread_mutex_t access_mutex;
+
+int has_all_res() {
+    for (int i = 0; i < n_rec; ++i) {
+        if (needed_tokens[i] && !my_tokens[i])
+            return 0;
+    }
+    return 1;
+}
 
 void init() {
     printf("Iniciando o nó %d\n", self);
     all_finalized = 0;
-    requesting = 0;
-    pthread_mutex_init(&access_mutex, NULL);
+    using_token = 0;
+//    pthread_mutex_init(&access_mutex, NULL);
+    my_tokens = malloc(sizeof(int) * n_rec);
+    needed_tokens = malloc(sizeof(int) * n_rec);
     queue = malloc(sizeof(int *) * n_procs);
     for (int k = 0; k < n_rec; ++k) {
         my_tokens[k] = 0;
+        needed_tokens[k] = 0;
     }
     for (int i = 0; i < n_procs; ++i) {
         queue[i] = malloc(sizeof(int) * n_rec);
@@ -43,63 +69,141 @@ void init() {
         }
         parent = -1;
     } else {
-        pthread_mutex_lock(&access_mutex);
+//        pthread_mutex_lock(&access_mutex);
         parent = elected_node;
     }
     next = -1;
+    printf("terminei de inicializar nó %d\n", self);
 }
 
-void release_cs();
 
-void send_token(int to);
+void send_ack(int response[3], int requester) {
 
-void send_request(int origin, int to);
+}
 
-void send_finalize();
 
-void receive_request_cs(int sj);
+void send_request_token(int origin, int parent) {
 
-void receive_token();
+}
 
-void request_cs();
+void send_token(int requirer) {
+    MPI_Send(&control_token, n_rec, MPI_INT, requirer, CONTROL_TOKEN_MESSAGE, MPI_COMM_WORLD);
+}
 
-void *receive(void *nil);
-
-void request_cs() {
-    requesting = 1;
-    printf("Nó %d quer entrar na seção crítico.\n", self);
+void receive_token_request(int origin) {
     if (parent != -1) {
-        send_request(self, parent);
-        parent = -1;
-
-        //espera token
-        pthread_mutex_lock(&access_mutex);
-
-    }
-    int random_sleep = (rand() % (3)) * 1000000;
-    printf("Nó %d entrou na seção irá dormir por %d segundos\n", self, random_sleep / 1000000);
-    usleep((__useconds_t) random_sleep);
-    release_cs();
-}
-
-
-int has_all_res(int requested[n_rec]) {
-    for (int i = 0; i < n_rec && requested[i] != -1; ++i) {
-        requested
+        send_request_token(origin, parent);
+        parent = origin;
+    } else {
+        parent = origin;
+        next = origin;
+        if (!using_token) {
+            send_token(origin);
+        }
     }
 }
 
-void request_resources(int requested[n_rec]) {
+void receive_inquire(int tokens[3], int requester) {
+    int response[3] = {-1, -1, -1};
+    int count = 0;
+    for (int i = 0; i < 3; ++i) {
+        int value = tokens[i];
+        if (value != -1) {
+            if (!needed_tokens[i])
+                response[count] = value; //coloca na resposta pro ack1 os tokens que não to usando
+            else {
+                for (int j = 0; j < n_proc; ++j) {
+                    if (queue[requester][j] == -1) {
+                        queue[requester][j] = value; //coloca em Q os que eu to usando pra ir pro ack2
+                    }
+                }
+            }
+
+        }
+    }
+    send_ack(response, requester);
+}
+
+void *listen_rec() {
+    while (all_finalized != n_procs) {
+        MPI_Status st;
+        int received;
+        MPI_Recv(&received, 1, MPI_INT, MPI_ANY_SOURCE, REQUEST_CONTROL_TOKEN, MPI_COMM_WORLD, &st);
+        printf("recebi de %d o valor %d\n", st.MPI_SOURCE, received);
+        if (received != -1)
+            receive_token_request(received);
+    }
+    return NULL;
+}
+
+void *listen_inq() {
+    while (all_finalized != n_procs) {
+        MPI_Status st;
+        int *received = malloc(sizeof(int) * 3);
+        MPI_Recv(received, 3, MPI_INT, MPI_ANY_SOURCE, INQ, MPI_COMM_WORLD, &st);
+        if (received[0] != -1)
+            receive_inquire(received, st.MPI_SOURCE);
+    }
+}
+
+void *listen_fin() {
+    while (all_finalized < n_procs) {
+        MPI_Status st;
+        int received;
+        MPI_Recv(&received, 1, MPI_INT, MPI_ANY_SOURCE, FINALIZE_MESSAGE, MPI_COMM_WORLD, &st);
+//        printf("Recebi finalize de %d\n", st.MPI_SOURCE);
+        all_finalized++;
+        MPI_Send(&all_finalized, 1, MPI_INT, st.MPI_SOURCE, ACK_FINALIZE, MPI_COMM_WORLD);
+    }
+    return NULL;
+}
+
+void send_finalize() {
+    for (int to = 0; to < n_procs; to++) {
+        printf("%d: Enviando finalize pra %d\n", self, to);
+        MPI_Send(&self, 1, MPI_INT, to, FINALIZE_MESSAGE, MPI_COMM_WORLD);
+        int meh;
+        MPI_Recv(&meh, 1, MPI_INT, to, ACK_FINALIZE, MPI_COMM_WORLD, NULL);
+        printf("%d: recebi ack finalize pra %d\n", self, to);
+        int void_value = -1;
+        printf("Enviando fake req pra %d\n", to);
+        MPI_Send(&void_value, 1, MPI_INT, to, REQUEST_CONTROL_TOKEN, MPI_COMM_WORLD);
+        int void_array[3] = {void_value, void_value, void_value};
+        printf("Enviando fake inq pra %d\n", to);
+        MPI_Send(&void_array, 3, MPI_INT, to, INQ, MPI_COMM_WORLD);
+    }
+}
+
+void use_resources(int requested[3]) {
     printf("Nó %d quer usar os recursos:", self);
-    for (int i = 0; i < n_rec && requested[i] != -1; ++i) {
+    for (int i = 0; i < 3; ++i) {
         printf(" %d", requested[i]);
+        needed_tokens[requested[i]] = 1;
     }
     printf("\n");
-    if (!has_all_res(requested)){
+    if (!has_all_res(requested)) { //se ele não possui todos os recursos
+        MPI_Send(&self, 1, MPI_INT, parent, REQUEST_CONTROL_TOKEN, MPI_COMM_WORLD);
+        control_token = malloc(sizeof(int) * n_rec);
+        MPI_Status st;
+        MPI_Recv(&control_token, n_rec, MPI_INT, MPI_ANY_SOURCE, CONTROL_TOKEN_MESSAGE, MPI_COMM_WORLD, &st);
+        using_token = 1;
+        for (int i = 0; i < n_rec; ++i) {
+            if (needed_tokens[i] && control_token[i] == -1) {//pegue todos os tokens que precisa e não tem
+                control_token[i] = self;
+                my_tokens[i] = 1;
+            } else if (control_token[i] == self && !needed_tokens[i]) {//devolva todos os que tem e não precisa
+                my_tokens[i] = 0;
+                control_token[i] = -1;
+            }
+        }
+        if (!has_all_res()){
+
+        }
 
     }
-    //seção crítica
-
+    if (control_token && next != -1) {
+        //envia control token
+    }
 }
 
 void execute() {//while gera um numero entre 0 e total_procs se gerou "meu numero" faço o pedido, dorme por um segundo
@@ -107,112 +211,42 @@ void execute() {//while gera um numero entre 0 e total_procs se gerou "meu numer
     while (count != 3) {
         int random = (rand() % (n_procs));
         if (self == random) {
-            int base_req = (rand() % (n_rec)); //primeiro recurso
-            int top_req = (rand() % (n_rec)); //ultimo recurso
-            top_req = top_req <= base_req ? base_req + 1 : top_req; // se top_req é menor que a base então ele é base +1
-            int requested_res[n_rec]; // array de recursos que eu quero
-            int j = 0;
-            for (int i = base_req; i < top_req; ++i) {
-                requested_res[j] = i; // quero o iésimo recurso a partir da base
-                j++;
-            }
-            requested_res[j] = -1; //para de procurar recurso
-            request_resources(requested_res);
+            int rand_idx = (rand() % (7));
+            use_resources(pv[rand_idx]);
             count++;
         }
         usleep(1 * 1000000);
     }
 }
 
+
 int main(int argc, char **argv) {
     srand(rand());
-    pthread_t rec_thread;
+    pthread_t rec_thread, inq_thread, fin_thread;
     MPI_Init(&argc, &argv);
 
     //inicializa
     MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &self);
     init();
+    MPI_Barrier(MPI_COMM_WORLD);
 
-    //levanta uma thread para ficar escutando requests
-    pthread_create(&rec_thread, NULL, receive, NULL);
+    //thread pra controle de finalize
+    pthread_create(&fin_thread, NULL, listen_fin, NULL);
+    //levanta uma thread para ficar escutando requests de control token
+    pthread_create(&rec_thread, NULL, listen_rec, NULL);
+    //levanta uma thread para ficar escutando request de INQUIRE
+    pthread_create(&inq_thread, NULL, listen_inq, NULL);
 
     //executa
     execute();
 
     //finaliza
     send_finalize();
+
+    pthread_join(fin_thread, NULL);
     pthread_join(rec_thread, NULL);
+    pthread_join(inq_thread, NULL);
     MPI_Finalize();
     return 0;
-}
-
-
-void receive_token() {
-    pthread_mutex_unlock(&access_mutex);
-    printf("Nó %d recebeu o token.\n", self);
-}
-
-void receive_finalize() {
-    all_finalized++;
-}
-
-void receive_request_cs(int sj) {
-    if (parent == -1) {
-        if (requesting) {
-            printf("%d é next de %d\n", sj, self);
-            next = sj;
-        } else {
-            send_token(sj);
-        }
-    } else {
-        send_request(sj, parent);
-    }
-    parent = sj;
-    printf(" Owner de %d é %d\n", self, parent);
-}
-
-void release_cs() {
-    printf("Nó %d liberando seção crítica\n", self);
-    pthread_mutex_unlock(&access_mutex);
-    requesting = 0;
-    if (next != -1) {
-        send_token(next);
-        pthread_mutex_lock(&access_mutex);
-        next = -1;
-    }
-}
-
-//função pra thread de receive
-void *receive(void *nil) {
-    while (all_finalized != n_procs) {
-        MPI_Status st;
-        int received;
-        MPI_Recv(&received, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &st);
-        printf("Look! Nó %d recebeu mensagem %d originária do nó %d através do nó %d\n", self, st.MPI_TAG,
-               received, st.MPI_SOURCE);
-        if (st.MPI_TAG == REQUEST_MESSAGE) {
-            receive_request_cs(received);
-        } else if (st.MPI_TAG == CONTROL_TOKEN_MESSAGE) {
-            receive_token();
-        } else if (st.MPI_TAG == FINALIZE_MESSAGE) {
-            receive_finalize();
-        } else {
-            printf("ERROR UNKNOWN MESSAGE RECEIVED: %c", st.MPI_TAG);
-        }
-    }
-}
-
-void send_finalize() {
-    for (int to = 0; to < n_procs; to++) {
-        MPI_Send(&self, 1, MPI_INT, to, FINALIZE_MESSAGE, MPI_COMM_WORLD);
-    }
-}
-
-void send_token(int to) {
-    MPI_Send(&self, 1, MPI_INT, to, CONTROL_TOKEN_MESSAGE, MPI_COMM_WORLD);
-}
-
-void send_request(int origin, int to) {
-    MPI_Send(&origin, 1, MPI_INT, to, REQUEST_MESSAGE, MPI_COMM_WORLD);
 }
